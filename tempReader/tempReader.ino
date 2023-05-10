d6eca637fe041d190f06d83857cb4bc27dffeef4
Greg <you@example.com>
2023-05-10 12:21:28 +0200

    12:21:28.20

--- tempReader/tempReader.ino
+++ tempReader/tempReader.ino
@@ -129,7 +129,7 @@ void SendUdpReport() {
       buf += String(tankTemp, 2);
       buf += F(", RoofToTank: ");      
       buf += String(roofToTankTemp, 2);
-      buf += F("TankTemp: ");
+      buf += F("Zone1Temp: ");
       buf += String(tankTemp, 2);
       
       buf += F(", last read: ");
