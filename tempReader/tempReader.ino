985312c861098bdb4693e549b817fadb6245ed79
Greg <you@example.com>
2023-05-10 09:38:49 +0200

    9:38:49.18

--- tempReader/tempReader.ino
+++ tempReader/tempReader.ino
@@ -90,11 +90,9 @@ void setup() {
 void loop() {
   currentMillis = millis();  // capture the latest value of millis()
 
-
   tankTemp = sensors.getTempC(tankTempAddres);  //Se obtiene la temperatura en °C del sensor 1
   roofToTankTemp = sensors.getTempC(fromRoofToTankAddress);
-  zo= sensors.getTempC(fromRoofToTankAddress);
-  
+  roof1ZoneTemp = sensors.getTempC(zone1Adress);  
   SendReadTempCommand();
 
 
