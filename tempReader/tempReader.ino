92bb94047ea321272aeacf728c9b7b12fceae25d
Greg <you@example.com>
2023-05-10 11:36:30 +0200

    11:36:30.15

--- tempReader/tempReader.ino
+++ tempReader/tempReader.ino
@@ -122,7 +122,7 @@ void SendUdpReport() {
   buf += F(", RoofToTank: ");
   buf += String(roofToTankTemp, 2);
   buf += F(", last read: ");
-  buf += String(micurrentMillisllis, 2);
+  buf += String(currentMillis, 2);
   char repBuff[buf.length()];
   buf.toCharArray(repBuff, buf.length());
 
