b545ac1fe68ff234f0531d8aa5a8a1196e89e495
Greg <you@example.com>
2023-05-11 12:35:02 +0200

    12:35:02.21

--- tempReader/tempReader.ino
+++ tempReader/tempReader.ino
@@ -202,6 +202,14 @@ void SendUdpReport() {
   return buf;
   
 }
+
+void printDigits(byte digits){
+  // utility function for digital clock display: prints colon and leading 0
+  Serial.print(":");
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits,DEC);   
+}
 void SendReadTempCommand() {
 
   if (currentMillis > _lastSensorsRequestTime + SensorCheckInterval) {
