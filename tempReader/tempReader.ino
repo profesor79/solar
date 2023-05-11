74cb9fa8560245cca253315ba24f0f04575fdbbb
Greg <you@example.com>
2023-05-11 20:11:21 +0200

    20:11:21.23

--- tempReader/tempReader.ino
+++ tempReader/tempReader.ino
@@ -10,7 +10,8 @@ const unsigned long SensorCheckInterval = 10000;
 const unsigned long SensorWaitInterval = 1500;
 const unsigned long FlushInterval = 60000;
 const unsigned long FlushTime = 10000;
-const float maxAverageTempDiffForFlush  = 115.00;
+const float maxAverageTempDiffForFlush  = 15.00;
+
 
 
 // define pins
