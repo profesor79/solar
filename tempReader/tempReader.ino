413e45672558015f93b4eb5cca92b954b8a7562b
Greg <you@example.com>
2023-05-11 11:52:56 +0200

    11:52:56.21

--- tempReader/tempReader.ino
+++ tempReader/tempReader.ino
@@ -30,12 +30,30 @@ unsigned int destinationPort = 8888;
 
 
 // temp sensors
-DallasTemperature sensors(&ourWire);                                               //Se declara una variable u objeto para nuestro sensor
+DallasTemperature sensors(&ourWire);                                                //Se declara una variable u objeto para nuestro sensor
 DeviceAddress tank2PumpAddres = { 0x28, 0x2A, 0xB7, 0xFE, 0xB0, 0x22, 0x8, 0x33 };  //dirección del sensor 1
 DeviceAddress fromRoofToTankAddress = { 0x28, 0xB7, 0xA4, 0xE6, 0xB0, 0x22, 0x8, 0xDE };
 DeviceAddress zone1Adress = { 0x28, 0x48, 0xD9, 0xF8, 0xB0, 0x22, 0x9, 0x7A };
-DeviceAddress tankAddress = {0x28, 0x51, 0x38, 0xDD, 0xB0, 0x22, 0x7, 0x4E, };
-DeviceAddress zone2Adress = {0x28, 0x36, 0x74, 0x3, 0xB1, 0x22, 0x9, 0x60, };
+DeviceAddress tankAddress = {
+  0x28,
+  0x51,
+  0x38,
+  0xDD,
+  0xB0,
+  0x22,
+  0x7,
+  0x4E,
+};
+DeviceAddress zone2Adress = {
+  0x28,
+  0x36,
+  0x74,
+  0x3,
+  0xB1,
+  0x22,
+  0x9,
+  0x60,
+};
 //DeviceAddress address2 = {0x28, 0xFF, 0x89, 0x3A, 0x1, 0x16, 0x4, 0xAF};//dirección del sensor 2
 //DeviceAddress address3 = {0x28, 0xFF, 0x23, 0x19, 0x1, 0x16, 0x4, 0xD9};//dirección del sensor 3
 float tank2Pump = -128;
@@ -100,9 +118,8 @@ void setup() {
   sensors.setResolution(tank2PumpAddres, tempBithDepth);
   sensors.setResolution(fromRoofToTankAddress, tempBithDepth);
   sensors.setResolution(zone1Adress, tempBithDepth);
-   sensors.setResolution(zone2Adress, tempBithDepth);
-   sensors.setResolution(tankAddress, tempBithDepth);
-
+  sensors.setResolution(zone2Adress, tempBithDepth);
+  sensors.setResolution(tankAddress, tempBithDepth);
 }
 
 void loop() {
@@ -110,14 +127,13 @@ void loop() {
   SendReadTempCommand();
   GetTemperatures();
   SendUdpReport();
-  
 }
 void GetTemperatures() {
 
   if (currentMillis > _lastSensorCheckTime + SensorWaitInterval) {
     if (!_temperatureWaseReadInThisCycle) {
       Serial.println("reading temperatures");
-      _lastSensorCheckTime = currentMillis;            
+      _lastSensorCheckTime = currentMillis;
       roofToTankTemp = sensors.getTempC(fromRoofToTankAddress);
       roof1ZoneTemp = sensors.getTempC(zone1Adress);
       roof2ZoneTemp = sensors.getTempC(zone2Adress);
@@ -129,38 +145,37 @@ void GetTemperatures() {
   }
 }
 void SendUdpReport() {
-  if(_udpSent){
+  if (_udpSent) {
     return;
   }
-  if (currentMillis > _lastSensorCheckTime + SensorWaitInterval & _lastSensorCheckTime>0) {
-    
-     Serial.println("udp send");
-      String buf;
-      buf += F("T: ");
-      buf += String(tankTemp, 2);
-      buf += F("TP: ");
-      buf += String(tank2Pump, 2);
-      buf += F(", RTT: ");      
-      buf += String(roofToTankTemp, 2);
-      buf += F(", Z1: ");
-      buf += String(roof1ZoneTemp, 2);
-      buf += F(", Z2: ");
-      buf += String(roof2ZoneTemp, 2);      
-      buf += F(", last read: ");
-      buf += String(_lastSensorCheckTime);
-      char repBuff[buf.length()];
-      buf.toCharArray(repBuff, buf.length());
+  if (currentMillis > _lastSensorCheckTime + SensorWaitInterval & _lastSensorCheckTime > 0) {
 
-      Udp.beginPacket(destinationIP, 8888);
-      Udp.write(repBuff);
-      Udp.endPacket();
-      _udpSent = true;
-    
+    Serial.println("udp send");
+    String buf;
+    buf += F("T: ");
+    buf += String(tankTemp, 2);
+    buf += F("TP: ");
+    buf += String(tank2Pump, 2);
+    buf += F(", RTT: ");
+    buf += String(roofToTankTemp, 2);
+    buf += F(", Z1: ");
+    buf += String(roof1ZoneTemp, 2);
+    buf += F(", Z2: ");
+    buf += String(roof2ZoneTemp, 2);
+    buf += F(", last read: ");
+    buf += String(_lastSensorCheckTime);
+    char repBuff[buf.length()];
+    buf.toCharArray(repBuff, buf.length());
+
+    Udp.beginPacket(destinationIP, 8888);
+    Udp.write(repBuff);
+    Udp.endPacket();
+    _udpSent = true;
   }
 }
 
 void SendReadTempCommand() {
- 
+
   if (currentMillis > _lastSensorsRequestTime + SensorCheckInterval) {
     Serial.println("sending get temp request");
     sensors.requestTemperatures();
