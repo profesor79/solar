2b08eac7360c881476f53b9fde5ddf5f38989f75
Greg <you@example.com>
2023-05-16 13:21:02 +0200

    13:21:02.22

--- tempReaderEtherenet/tempReaderEtherenet.ino/tempReaderEtherenet.ino.ino
+++ tempReaderEtherenet/tempReaderEtherenet.ino/tempReaderEtherenet.ino.ino
@@ -39,6 +39,27 @@ void loop() {
   }  
 }
 
+
+/*
+  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
+  routine is run between each time loop() runs, so using delay inside loop can
+  delay response. Multiple bytes of data may be available.
+*/
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}
+
+
 void SendUDPPacket(String message) {
   int len = message.length() + 1;
   char repBuff[len];
