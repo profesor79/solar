77ed95ca29e713dd6729b2e88095d9cf91ac017f
Greg <you@example.com>
2023-05-17 05:13:33 +0200

    5:13:33.23

--- tempReaderEtherenet/tempReaderEtherenet.ino/tempReaderEtherenet.ino.ino
+++ tempReaderEtherenet/tempReaderEtherenet.ino/tempReaderEtherenet.ino.ino
@@ -75,7 +75,7 @@ void SendUDPPacket(String message) {
   String name = message.substring(0,splitAt);
   String value = message.substring(splitAt, message.length());
   Serial.println(name);
-  Serial.println(vaule);
+  Serial.println(value);
   
 
 }
