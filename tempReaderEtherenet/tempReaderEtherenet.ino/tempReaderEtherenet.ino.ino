2988bb93057d2461886c73270feccf61f07ac222
Greg <you@example.com>
2023-05-17 14:50:31 +0200

    14:50:31.24

--- tempReaderEtherenet/tempReaderEtherenet.ino/tempReaderEtherenet.ino.ino
+++ tempReaderEtherenet/tempReaderEtherenet.ino/tempReaderEtherenet.ino.ino
@@ -40,7 +40,7 @@ void setup() {
   publishConfig("RoofZone1");
 }
 
-const String configBase = "{\"name\": \"__\",\"unit_of_measurement\": \"C\",\"state_topic\": \"homeassistant/sensor/greg___\",\"icon\": \"mdi:temperature-celsius\" }";
+const String configBase = "{\"name\": \"__\",\"unit_of_measurement\": \"C\",\"state_topic\": \"homeassistant/sensor/__\",\"icon\": \"mdi:temperature-celsius\" }";
 
 void SendConfiguration() {
 
