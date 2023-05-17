6f301d11907a1221b836cfe2a9f056d0da14a8e1
Greg <you@example.com>
2023-05-17 12:03:32 +0200

    12:03:32.20

--- tempReaderEtherenet/tempReaderEtherenet.ino/tempReaderEtherenet.ino.ino
+++ tempReaderEtherenet/tempReaderEtherenet.ino/tempReaderEtherenet.ino.ino
@@ -68,7 +68,7 @@ WaterPump
 
 void publishConfig(String name){
 String c = configBase;
-c.replace("___", "_<name);
+c.replace("___", ("_" + name));
 String topic ="homeassistant/sensor/greg_" + name+"/config";
 client.publish(topic, c);    
 Serial.println(topic);
