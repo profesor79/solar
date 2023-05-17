06bb410695d9508dab76589fd6d5b5b621528f22
Greg <you@example.com>
2023-05-17 05:39:49 +0200

    5:39:49.19

--- tempReaderEtherenet/tempReaderEtherenet.ino/tempReaderEtherenet.ino.ino
+++ tempReaderEtherenet/tempReaderEtherenet.ino/tempReaderEtherenet.ino.ino
@@ -90,9 +90,9 @@ void SendUDPPacket(String message) {
   {
   String name = message.substring(0,splitAt);
   String value = message.substring(splitAt+1, message.length());
-  Serial.print("Name: " + name);
-    Serial.println(", V: " + value);
-   //client.publish("homeassistant/sensors/" + name, value);    
+//  Serial.print("Name: " + name);
+  //  Serial.println(", V: " + value);
+   client.publish("homeassistant/sensors/" + name, value);    
   }
   
 
