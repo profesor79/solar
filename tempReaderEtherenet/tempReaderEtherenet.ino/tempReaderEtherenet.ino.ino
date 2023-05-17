aa49aadf988a039198d4d9071c866c68c4b89c1d
Greg <you@example.com>
2023-05-17 06:20:09 +0200

    6:20:09.24

--- tempReaderEtherenet/tempReaderEtherenet.ino/tempReaderEtherenet.ino.ino
+++ tempReaderEtherenet/tempReaderEtherenet.ino/tempReaderEtherenet.ino.ino
@@ -16,7 +16,7 @@ bool stringComplete = false;  // whether the string is complete
 unsigned long currentMillis = 0;  // stores the value of millis() in each iteration of loop()
 unsigned long lastConfigSendTime= 0;
 unsigned long configSendInterval= 300000;
-bool configSent = true;
+bool configSent = false;
 
 EthernetClient net;
 MQTTClient client;
