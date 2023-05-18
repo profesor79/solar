519c9b31b01c8b81855e21658539d5e10a97af47
Greg <you@example.com>
2023-05-18 08:04:41 +0200

    8:04:41.17

--- tempReader/tempReader.ino
+++ tempReader/tempReader.ino
@@ -142,7 +142,7 @@ bool tmpIsPumpRunning = (digitalRead(solarPumpVoltagePin) == LOW);
 // water pump is enabled always and controlled by pressure switch
   bool tmpIsVoltage == (digitalRead(waterPumpVoltagePin) == LOW);
 
-  if (_waterPumpRunning) {
+  if (tmpIsVoltage!=) {
     SwitchOffSolarPump();
     _solarPumpRunning = false;
     WriteLogEntry("WaterPump:1");
