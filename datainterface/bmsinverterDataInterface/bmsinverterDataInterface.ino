df7b183764ed2715c3b57b5ae39caf221a8acf95
Greg <you@example.com>
2023-06-29 19:15:46 +0200

    aa

--- datainterface/bmsinverterDataInterface/bmsinverterDataInterface.ino
+++ datainterface/bmsinverterDataInterface/bmsinverterDataInterface.ino
@@ -1,33 +1,19 @@
-#include <SoftwareSerial.h>
-
-const byte bmsRxPin = 7;
-const byte bmsTxPin = 8;
-
-SoftwareSerial bmsSerial(bmsRxPin, bmsTxPin);
-
-const byte inverterRxPin = 6;
-const byte inverterTxPin = 5;
-//SoftwareSerial inverterSerial(inverterRxPin, inverterTxPin);
-
-
-
-
 String results[25] = {
-  "gridVoltage_0",        
-  "gridFrequency_1",      //1
-  "outputVoltage_2",      //2
-  "outputFrequency_3",    //3
-  "outputPowerApparent_4",//4
-  "outputPowerActive_5",  //5
-  "outputLoadPercent_6",  //6
-  "busVoltage_7",         //7
-  "batteryVoltage_8",//8
-  "batteryChargingCurrent_9",//9
-  "batteryCapacity_10",//10
-  "temperature_11",//11
-  "pvBatteryCurrent_12",//12
-  "pvInputVoltage_13",//13
-  "batteryVoltageSCC_14",//14
+  "gridVoltage",        
+  "gridFrequency",      //1
+  "outputVoltage",      //2
+  "outputFrequency",    //3
+  "outputPowerApparent",//4
+  "outputPowerActive",  //5
+  "outputLoadPercent",  //6
+  "busVoltage",         //7
+  "batteryVoltage",//8
+  "batteryChargingCurrent",//9
+  "batteryCapacity",//10
+  "temperature",//11
+  "pvBatteryCurrent",//12
+  "pvInputVoltage",//13
+  "batteryVoltageSCC",//14
   "batteryDischargeCurrent_15",  //15
     "addSBUPriorityVersion_16",
     "configChanged_17",
@@ -46,75 +32,96 @@ String _lastRequestedCommand = "-";
 
 String _nextCommandNeeded;
 
-//String _nextCommandNeeded = "";
-bool _allMessagesUpdated = false;
-
  
 
+String inputString = "";      // a String to hold incoming data
+bool stringComplete = false;  // whether the string is complete
+
+unsigned long currentMillis = 0;  // stores the value of millis() in each iteration of loop()
+unsigned short crc;
+ 
+
+const unsigned long CheckInterval = 1000;
+unsigned long _lastCheckTime;
 void setup() {
-  // put your setup code here, to run once:
- // Define pin modes for TX and RX
-  pinMode(bmsRxPin, INPUT);
-  pinMode(bmsTxPin, OUTPUT);
-  pinMode(inverterRxPin, INPUT);
-  pinMode(inverterTxPin, OUTPUT);
   Serial.begin(9600);
   Serial1.begin(2400);
-  
-  //inverterSerial.begin(2400);
   Serial.println("starting");
+    // reserve 200 bytes for the inputString:
+  inputString.reserve(200);
 
+}
+
+
+void serialEvent1() {
+  while (Serial1.available()) {
+    // get the new byte:
+    char inChar = (char)Serial1.read();
+    // add it to the inputString:
+    inputString += inChar;
+   
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\r') {
+      stringComplete = true;
+    }
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-qpigs();
-delay(500);
+  currentMillis = millis();  // capture the latest value of millis()
+  if(currentMillis-_lastCheckTime>CheckInterval){
+    qpigs();
+    _lastCheckTime=currentMillis;
+    }
+  if (stringComplete) {
+   ProcessSerial1Data();
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
+  } 
 }
+
+void ProcessSerial1Data(){
+   int counter = 0;
+    int len = inputString.length();
+    String command ="";
+    for(int i=0;i<len;i++){
+      if(counter>13) continue;
+      if(inputString[i]==' '){
+       String buffer=results[counter];
+       buffer+=":";
+       buffer+=command;
+       buffer+="\r\n";
+    
+        Serial.print(results[counter]);
+        Serial.print(":");
+        
+        Serial.println(command);        
+      
+      delay(20);
+
+
+      command ="";
+      counter++;
+      }else{
+        //Serial.println(incomingString[i]);
+        command +=inputString[i];
+        }
+      }    
+  }
 
 
 void qpigs()
 {
-  
-  byte c; 
-    _commandBuffer = "";   
-_nextCommandNeeded="QPIGS";
-    unsigned short crc = cal_crc_half((byte*)_nextCommandNeeded.c_str(), _nextCommandNeeded.length());  
-    Serial.print("command to execute: ");
-    Serial.println(_nextCommandNeeded);
+    _nextCommandNeeded="QPIGS";
+  crc = cal_crc_half((byte*)_nextCommandNeeded.c_str(), _nextCommandNeeded.length());  
   
     Serial1.print(_nextCommandNeeded);
     Serial1.print((char)((crc >> 8) & 0xFF));
     Serial1.print((char)((crc >> 0) & 0xFF));
     Serial1.print("\r");   
-
-  
-  
-delay(100);
-Serial1.setTimeout(500); 
-Serial1.read(); 
-   String incomingString = Serial1.readStringUntil('\r');
-Serial.println(incomingString );
-    int counter = 0;
-    int len = incomingString.length();
-    String command ="";
-    for(int i=0;i<len;i++){
-      if(counter>13) continue;
-      if(incomingString[i]==' '){
-      Serial.print("counter: ");
-        Serial.print(counter);
-        Serial.print(" command: ");
-        Serial.print(results[counter]);
-        Serial.print(" value: ");
-      Serial.println(command);
-      command ="";
-      counter++;
-      }else{
-        //Serial.println(incomingString[i]);
-        command +=incomingString[i];
-        }
-      }    
 }
 
 
